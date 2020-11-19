287a3e4806bd9279154840178fd79e8577071692
jonabraham0 <70244078+jonabraham0@users.noreply.github.com>
2020-11-19 10:44:13 -0800

    Update pw.py

--- pw.py
+++ pw.py
@@ -5,9 +5,15 @@ PASSWORDS = {'email': 'Jon123!@#',
 			'blog': 'Roger456$%^', 
 			'luggage': '12345'}
 
-import sys
+import sys, pyperclip
 if len(sys.argv) < 2:
 	print('Usage: python pw.py [account] - copy account password')
 	sys.exit()
 
 account = sys.argv[1]     # first command line arg is the account name 
+
+if account in PASSWORDS:
+	pyperclip.copy(PASSWORDS[account])
+	print(f"Password for {account} copied to clipboard.")
+else:
+	print(f"There is no account named {account}")
